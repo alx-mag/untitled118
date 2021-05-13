a2e34ad95287f545a2e4455869ec6966e47fd825
Alexander Magomedov <magomedov@haulmont.com>
2021-05-13 23:04:45 +0400

    Initial commit

---
+++ src/test/java/com/company/untitled118/Untitled118ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.untitled118;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Untitled118ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
